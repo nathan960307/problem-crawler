ef6d5fd550d84de2de812b618aa0a4b7912404fb
nathan <nathan960307@gmail.com>
2025-06-14 17:56:08 +0900

    (0614) feat : Jsoup 라이브러리 추가(1756)

---
+++ src/main/java/com/nathan/problemcrawler/crawler/ProgrammersCrawler.java
@@ -0,0 +1,20 @@
+package com.nathan.problemcrawler.crawler;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class ProgrammersCrawler {
+
+    public static void main(String[] args) throws Exception {
+        String url = "https://school.programmers.co.kr/learn/challenges";
+        Document doc = Jsoup.connect(url).get();
+
+        Elements titles = doc.select(".challenge-card__title");
+
+        for (Element title : titles) {
+            System.out.println("문제명: " + title.text());
+        }
+    }
+}
---
+++ src/test/java/com/nathan/problemcrawler/ProblemCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathan.problemcrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProblemCrawlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
